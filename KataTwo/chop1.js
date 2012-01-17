0a1df35cfe8d651a3f1b40ae02b04807f6a9d1db
h3nd3r <hender.sara@gmail.com>
2012-01-16 18:25:37 -0800

    Add Chop2 to test, and clean Chop1 a bit

--- KataTwo/chop1.js
+++ KataTwo/chop1.js
@@ -1,19 +1,11 @@
 /*
 ** Binary Search, Day 1
 */
-
-// avoid floating point results when dividing
-function divide ( n, d ) {
-    var r = n % d;
-    return ( n - r ) / d;
-};
-
-exports.chop1 = function ( target, sorted_array ) {
+exports.chop = function ( target, sorted_array ) {
 
   var l = 0; var r = sorted_array.length - 1;  var mid;
-
   while(r >= l) {
-    mid = divide((l + r), 2);
+    mid = Math.floor((l+r)/2);
     if (sorted_array[mid] < target)
       l = mid + 1;
     else if (sorted_array[mid] > target)
--- KataTwo/test.js
+++ KataTwo/test.js
@@ -1,29 +1,58 @@
 
-var chop1 = require('./chop1.js');
+var chop = require('./chop1.js');
 
 function assert_equal(expected, result){
   console.log(expected == result);
 }
 
-assert_equal(-1, chop1.chop1(3, []));
-assert_equal(-1, chop1.chop1(3, [1]));
-assert_equal(0,  chop1.chop1(1, [1]));
+console.log("Chop1:");
 
-assert_equal(0,  chop1.chop1(1, [1, 3, 5]));
-assert_equal(1,  chop1.chop1(3, [1, 3, 5]));
-assert_equal(2,  chop1.chop1(5, [1, 3, 5]));
-assert_equal(-1, chop1.chop1(0, [1, 3, 5]));
-assert_equal(-1, chop1.chop1(2, [1, 3, 5]));
-assert_equal(-1, chop1.chop1(4, [1, 3, 5]));
-assert_equal(-1, chop1.chop1(6, [1, 3, 5]));
+assert_equal(-1, chop.chop(3, []));
+assert_equal(-1, chop.chop(3, [1]));
+assert_equal(0,  chop.chop(1, [1]));
 
-assert_equal(0,  chop1.chop1(1, [1, 3, 5, 7]));
-assert_equal(1,  chop1.chop1(3, [1, 3, 5, 7]));
-assert_equal(2,  chop1.chop1(5, [1, 3, 5, 7]));
+assert_equal(0,  chop.chop(1, [1, 3, 5]));
+assert_equal(1,  chop.chop(3, [1, 3, 5]));
+assert_equal(2,  chop.chop(5, [1, 3, 5]));
+assert_equal(-1, chop.chop(0, [1, 3, 5]));
+assert_equal(-1, chop.chop(2, [1, 3, 5]));
+assert_equal(-1, chop.chop(4, [1, 3, 5]));
+assert_equal(-1, chop.chop(6, [1, 3, 5]));
 
-assert_equal(3,  chop1.chop1(7, [1, 3, 5, 7]));
-assert_equal(-1, chop1.chop1(0, [1, 3, 5, 7]));
-assert_equal(-1, chop1.chop1(2, [1, 3, 5, 7]));
-assert_equal(-1, chop1.chop1(4, [1, 3, 5, 7]));
-assert_equal(-1, chop1.chop1(6, [1, 3, 5, 7]));
-assert_equal(-1, chop1.chop1(8, [1, 3, 5, 7]));
+assert_equal(0,  chop.chop(1, [1, 3, 5, 7]));
+assert_equal(1,  chop.chop(3, [1, 3, 5, 7]));
+assert_equal(2,  chop.chop(5, [1, 3, 5, 7]));
+
+assert_equal(3,  chop.chop(7, [1, 3, 5, 7]));
+assert_equal(-1, chop.chop(0, [1, 3, 5, 7]));
+assert_equal(-1, chop.chop(2, [1, 3, 5, 7]));
+assert_equal(-1, chop.chop(4, [1, 3, 5, 7]));
+assert_equal(-1, chop.chop(6, [1, 3, 5, 7]));
+assert_equal(-1, chop.chop(8, [1, 3, 5, 7]));
+
+console.log("\nChop2:");
+var chop = require('./chop2.js');
+assert_equal(-1, chop.chop(3, []));
+assert_equal(-1, chop.chop(3, [1]));
+assert_equal(0,  chop.chop(1, [1]));
+
+assert_equal(0,  chop.chop(1, [1, 3, 5]));
+assert_equal(1,  chop.chop(3, [1, 3, 5]));
+assert_equal(2,  chop.chop(5, [1, 3, 5]));
+assert_equal(-1, chop.chop(0, [1, 3, 5]));
+assert_equal(-1, chop.chop(2, [1, 3, 5]));
+assert_equal(-1, chop.chop(4, [1, 3, 5]));
+assert_equal(-1, chop.chop(6, [1, 3, 5]));
+
+assert_equal(0,  chop.chop(1, [1, 3, 5, 7]));
+assert_equal(1,  chop.chop(3, [1, 3, 5, 7]));
+assert_equal(2,  chop.chop(5, [1, 3, 5, 7]));
+
+assert_equal(3,  chop.chop(7, [1, 3, 5, 7]));
+assert_equal(-1, chop.chop(0, [1, 3, 5, 7]));
+assert_equal(-1, chop.chop(2, [1, 3, 5, 7]));
+assert_equal(-1, chop.chop(4, [1, 3, 5, 7]));
+assert_equal(-1, chop.chop(6, [1, 3, 5, 7]));
+assert_equal(-1, chop.chop(8, [1, 3, 5, 7]));
+
+
